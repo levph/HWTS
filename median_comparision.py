f9833269deb5b5066e4cbbe3ede3c00cbc3b7f72
Lev <levvph@gmail.com>
2023-12-17 21:38:31 +0200

    results for most iscas netlists

--- median_comparision.py
+++ median_comparision.py
@@ -5,6 +5,7 @@ import numpy as np
 import re
 import argparse
 
+
 def process_netlist_name_and_limit(df_path):
     # Get the parent directory of the given file path
     parent_directory = os.path.dirname(df_path)
@@ -12,7 +13,7 @@ def process_netlist_name_and_limit(df_path):
     # Extract the last component (immediate parent directory) from the path
     immediate_parent_directory = os.path.basename(parent_directory)
 
-    #extracts the limit
+    # extracts the limit
     limit = 0
     df_basename = os.path.splitext(os.path.basename(df_path))[0]
     if "_inf_" in df_basename:
@@ -30,6 +31,7 @@ def process_netlist_name_and_limit(df_path):
 
     return immediate_parent_directory, limit
 
+
 if __name__ == "__main__":
     # parsing command line arguments
     parser = argparse.ArgumentParser()
@@ -45,21 +47,25 @@ if __name__ == "__main__":
     diff_column = None
 
     if (df1_limit >= df2_limit and df2_limit != -1) or df1_limit == -1:
-        diff_column = abs(df2['MEDIAN'] - df1['MEDIAN']) / df1['MEDIAN']
-        #diff_column2 = (df1['MEDIAN'] - df2['MEDIAN']) / df1['MEDIAN']
+        diff_column = abs(df2['MEAN'] - df1['MEAN']) / df1['MEAN']
+
+        # diff_column2 = (df1['MEDIAN'] - df2['MEDIAN']) / df1['MEDIAN']
     else:
-        diff_column = (df1['MEDIAN'] - df2['MEDIAN']) / df2['MEDIAN']
-        #diff_column2 = (df2['MEDIAN'] - df1['MEDIAN']) / df2['MEDIAN']
+        diff_column = (df1['MEAN'] - df2['MEAN']) / df2['MEAN']
+
+        # diff_column2 = (df2['MEDIAN'] - df1['MEDIAN']) / df2['MEDIAN']
 
     diff_column3 = abs(df2['SIZE'] - df1['SIZE'])
 
-    new_df = pd.DataFrame({ 'GATE_NAME': df1['GATE_NAME'], 'GATE_TYPE': df1['GATE_TYPE'],
-                            'MEDIAN_DIFF': diff_column, 'SIZE_DIFF':diff_column3}).sort_values(by='MEDIAN_DIFF', ascending=False)
+    new_df = pd.DataFrame({'GATE_NAME': df1['GATE_NAME'], 'GATE_TYPE': df1['GATE_TYPE'],
+                           'MEAN_DIFF': diff_column, 'SIZE_DIFF': diff_column3, 'LIM2': df1['MEAN'],
+                           'LIM4': df2['MEAN']}).sort_values(by='MEAN_DIFF',
+                                                             ascending=False)
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
         print(new_df)
 
+    total = len(diff_column)
+    sus_amnt = len([a for a in diff_column if a > 1])
 
-
-
-
+    print("\nSuspicious wires (mean_diff>1): " + str(sus_amnt) + "/" + str(total))
